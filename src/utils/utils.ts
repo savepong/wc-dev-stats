49458798a18b83e338955b1a24543cda70f92387
Savepong <savepong.com@gmail.com>
2021-06-06 19:10:17 +0700

    add formatAlt utility

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,12 @@
 export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+export function formatAlt(text: string, username?: string): string {
+  return username ? `${capitalize(username)}' ${text}` : text;
+}
+
+function capitalize(str) {
+  const lower = str.toLowerCase();
+  return str.charAt(0).toUpperCase() + lower.slice(1);
+}
